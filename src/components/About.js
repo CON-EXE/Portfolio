d3f4b46aea286d1a5834764b8bb45e1e8da24b06
Connor Hall <connrhall204@gmail.com>
2025-12-08 10:32:06 -0600

    Updated About Me Content

--- src/components/About.js
+++ src/components/About.js
@@ -5,20 +5,18 @@ function About() {
                 <div className="gap about-content">
                     <div className="about-column about-text">
                         <p>
-                            I'm an up and coming software developer with a fair bit of 
-                            experience using HTML, CSS, and JavaScript. On top of that 
-                            I have some past experience with Java. I take great pride 
+                            I'm an up and coming full stack developer with solid experience 
+                            in both front end and back end development. I take great pride 
                             in my work and always strive to do the best I can in everything 
                             I do. On top of that I have a passion for problem solving. 
                             If you present me with a challenge, I'll almost always find 
                             a way to get through it. 
                         </p>
                         <p>
-                            When I'm not busy racking my brain trying to figure out how to 
-                            center a div for the fiftieth time, you'll probably find me playing 
-                            with my dog, Jasper, reading, gaming, or mountain biking in the 
-                            summer. I'm a huge fan of Brandon Sanderson novels and games 
-                            like Deep Rock Galactic and Slay the Spire.
+                            When I'm not busy with projects or learning new tech, you'll probably 
+                            find me playing with my dog, Jasper, reading, gaming, or mountain biking 
+                            in the summer. I'm a huge fan of Brandon Sanderson novels and games like 
+                            Deep Rock Galactic and Terraria.
                         </p>
                         <p>
                             Want to know more? <a href="#contact"><span>Get in touch!</span></a>
